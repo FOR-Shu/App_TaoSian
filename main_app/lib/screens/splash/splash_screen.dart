283885195fede081d5c01fbaa11f29b6044da416
FOR-Shu <shuyuanchuang2020@gmail.com>
2021-07-18 01:32:45 +0800

    welcome page.

--- main_app/lib/screens/splash/splash_screen.dart
+++ main_app/lib/screens/splash/splash_screen.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:main_app/screens/splash/components/body.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:main_app/screens/home/home_screen.dart';
 import 'package:main_app/size_config.dart';
 
-class SplashScreen extends StatelessWidget {
-  static String routeName = "/splash";
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    var d = Duration(seconds: 3);
+    //delay 3 seconds to next page
+    Future.delayed(d, () {
+      // to next page and close this Page()
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+        builder: (context) {
+          return HomeScreen();
+        },
+      ), (route) => false);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     // You have to call it on your starting screen
     SizeConfig().init(context);
     return Scaffold(
-      body: Body(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.png'),
+          ),
+        ),
+        child: Align(
+          alignment: FractionalOffset(0.5, 0.45),
+          child: SvgPicture.asset('assets/images/logo.svg'),
+        ),
+      ),
     );
   }
 }
