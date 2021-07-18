8aee9de6ec61d55199b6e3d07a1279bc5c46fa99
FOR-Shu <shuyuanchuang2020@gmail.com>
2021-07-18 22:16:39 +0800

    - icon 新增 - nav bar 更新

--- main_app/lib/screens/home/home_screen.dart
+++ main_app/lib/screens/home/home_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:main_app/components/coustom_bottom_nav_bar.dart';
+
+import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
+  static String routeName = "/home";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Body(),
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+    );
   }
 }
