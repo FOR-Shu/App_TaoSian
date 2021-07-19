b496c0329d5f35bc9f03d3aaea7c2189d5f3bc55
FOR-Shu <shuyuanchuang2020@gmail.com>
2021-07-20 00:48:53 +0800

    - 所有頁面基礎設定完畢

--- main_app/lib/screens/home/components/body.dart
+++ main_app/lib/screens/home/components/body.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-
 import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: getProportionateScreenHeight(20)),
-            SizedBox(height: getProportionateScreenWidth(10)),
-            SizedBox(height: getProportionateScreenWidth(30)),
-            SizedBox(height: getProportionateScreenWidth(30)),
-          ],
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Background.png"),
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fitWidth,
+          ),
         ),
       ),
     );
--- main_app/lib/screens/setting/components/body.dart
+++ main_app/lib/screens/setting/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:main_app/constants.dart';
+import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
   @override
