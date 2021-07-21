313543e55ba38e0a969fcef11038d48c13a72c82
FOR-Shu <shuyuanchuang2020@gmail.com>
2021-07-21 11:11:52 +0800

    - qrscan home finish

--- main_app/lib/screens/home/components/body.dart
+++ main_app/lib/screens/home/components/body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:main_app/constants.dart';
 import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+          print('press...');
+        },
+        backgroundColor: kPrimaryColor,
+        child: Icon(
+          Icons.add,
+          size: 40.0,
+          color: Colors.white,
+        ),
+      ),
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
@@ -13,6 +30,33 @@ class Body extends StatelessWidget {
             alignment: Alignment.topCenter,
             fit: BoxFit.fitWidth,
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: size.height * .1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 45),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 30, 30, 30),
+                              fontWeight: FontWeight.w900,
+                              fontFamily: 'Noto_Serif_TC',
+                              fontSize: 30,
+                            ),
+                            text: "Hi, User."),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
--- main_app/lib/screens/qrscan/components/body.dart
+++ main_app/lib/screens/qrscan/components/body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:main_app/constants.dart';
 import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+          print('press...');
+        },
+        backgroundColor: kPrimaryColor,
+        child: Icon(
+          Icons.add,
+          size: 40.0,
+          color: Colors.white,
+        ),
+      ),
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
@@ -13,6 +30,63 @@ class Body extends StatelessWidget {
             alignment: Alignment.topCenter,
             fit: BoxFit.fitWidth,
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: size.height * .1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 45),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 30, 30, 30),
+                              fontWeight: FontWeight.w900,
+                              fontFamily: 'Noto_Serif_TC',
+                              fontSize: 30,
+                            ),
+                            text: "食品輸入"),
+                        TextSpan(
+                          text: "\n 點擊卡片編輯內容",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 50, 50, 50),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Noto_Serif_TC',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    //塞間隔
+                    child: Container(),
+                  ),
+                  SizedBox(
+                    child: Align(
+                      alignment: Alignment(0.60, 0.80),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary:
+                              Colors.teal.shade300, //background color of button
+                          elevation: 8, //elevation of button
+                        ),
+                        onPressed: () {
+                          // Add your onPressed code here!
+                          print('press...');
+                        },
+                        child: const Text('匯入庫存'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
